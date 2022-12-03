ab68845cca3d08a551feadc4567f51211e824f62
Jon Jackson <ocskier@gmail.com>
2022-12-03 12:11:03 -0500

    Adds full calc

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,17 @@ const isClickableEls = document.querySelectorAll(".is-clickable");
 const calc = new Calculator();
 
 let isNum1Selected = false;
+let isNum2Selected = false;
 let isOperatorSelected = false;
+
+const resetCalc = () => {
+  calc.num1 = "";
+  calc.num2 = "";
+  calc.operator = "";
+  isNum1Selected = false;
+  isNum2Selected = false;
+  isOperatorSelected = false;
+};
 
 isClickableEls.forEach((el) => {
   el.addEventListener("click", function () {
@@ -13,8 +23,8 @@ isClickableEls.forEach((el) => {
     }
     if (!isOperatorSelected && /[+\-*\/]/g.test(userInput)) {
       if (calc.num1) {
-        calc.operator = userInput;
         isNum1Selected = true;
+        calc.operator = userInput;
         isOperatorSelected = true;
         return calc.operator;
       }
@@ -22,6 +32,15 @@ isClickableEls.forEach((el) => {
     if (isNum1Selected && isOperatorSelected && !isNaN(Number(userInput))) {
       calc.num2 += Number(userInput);
       return calc.num2;
+    }
+    if (isNum1Selected && isOperatorSelected && /[=]/g.test(userInput)) {
+      if (calc.num1 && calc.num2) {
+        isNum2Selected = true;
+        console.log(
+          `${calc.num1}${calc.operator}${calc.num2}=${calc.getAnswer()}`
+        );
+        resetCalc();
+      }
     }
   });
 });
