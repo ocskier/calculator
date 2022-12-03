5b0dc9c09d8a8ba3d279c0b1f8bd0bfae947a299
Jon Jackson <ocskier@gmail.com>
2022-12-03 10:48:39 -0500

    Adds a class for cal

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,1 +1,30 @@
-console.log('loaded');
+class Calculator {
+  constructor() {
+    this.num1 = undefined;
+    this.num2 = undefined;
+    this.operator = undefined;
+  }
+  setNum1(num) {
+    this.num1 = num;
+  }
+  setNum2(num) {
+    this.num2 = num;
+  }
+  setOperator(action) {
+    this.operator = action;
+  }
+  getAnswer() {
+    switch (this.operator) {
+      case "+":
+        return this.num1 + this.num2;
+      case "-":
+        return this.num1 - this.num2;
+      case "*":
+        return this.num1 * this.num2;
+      case "/":
+        return this.num1 / this.num2;
+      default:
+        return undefined;
+    }
+  }
+}
